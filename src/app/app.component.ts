1633bdd397f6ce2c6d61fdbbd034615172e4cf73
Larissa Brasil <larissabrasil009@gmail.com>
2024-12-24 09:50:29 -0300

    feat: adicionando cabeçalho e atualizando CSS no componente inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SobreNosComponent } from "./sobre-nos/sobre-nos.component";
+import { ContatoComponent } from "./contato/contato.component";
+import { InicialComponent } from "./inicial/inicial.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SobreNosComponent, ContatoComponent, InicialComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'dorothypiaui.github.io';
+  title = 'Dorothy Piauí';
 }
