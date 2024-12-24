fb3cd07aca05abe68f5e71b0601a45c3ac63477d
Larissa Brasil <larissabrasil009@gmail.com>
2024-12-24 11:09:49 -0300

    feat: adicionar novos componentes Participacoes, Contato, Projetos e Sobre Nos vazios

---
+++ src/app/participacoes/participacoes.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-participacoes',
+  imports: [],
+  templateUrl: './participacoes.component.html',
+  styleUrl: './participacoes.component.css'
+})
+export class ParticipacoesComponent {
+
+}
