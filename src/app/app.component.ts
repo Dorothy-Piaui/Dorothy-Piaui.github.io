d0ea9861ac9cc263d057682b6f7c2c9b5005b5b8
Larissa Brasil <larissabrasil009@gmail.com>
2024-12-26 10:57:54 -0300

    config: configurando rotas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,19 @@ import { RouterOutlet } from '@angular/router';
 import { SobreNosComponent } from "./sobre-nos/sobre-nos.component";
 import { ContatoComponent } from "./contato/contato.component";
 import { InicialComponent } from "./inicial/inicial.component";
+import { ParticipacoesComponent } from './participacoes/participacoes.component';
+import { ProjetosComponent } from "./projetos/projetos.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, SobreNosComponent, ContatoComponent, InicialComponent],
+  imports: [
+    RouterOutlet,
+    SobreNosComponent,
+    ContatoComponent,
+    InicialComponent,
+    ParticipacoesComponent,
+    ProjetosComponent
+],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
+import { InicialComponent } from './inicial/inicial.component';
+import { ProjetosComponent } from './projetos/projetos.component';
+import { SobreNosComponent } from './sobre-nos/sobre-nos.component';
+import { ParticipacoesComponent } from './participacoes/participacoes.component';
+import { ContatoComponent } from './contato/contato.component';
 import { Routes } from '@angular/router';
+import { Component } from '@angular/core';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: InicialComponent },
+    { path: 'projetos', component: ProjetosComponent },
+    { path: 'sobre-nos', component: SobreNosComponent },
+    { path: 'participacoes', component: ParticipacoesComponent },
+    { path: 'contato', component: ContatoComponent }
+];
