5b80e0f7e0bc321cd507f92570543b9c6d7cc990
Larissa Brasil <larissabrasil009@gmail.com>
2024-12-26 20:34:16 -0300

    refactor: removendo importações não utilizadas e limpando estilos CSS

--- src/app/inicial/inicial.component.spec.ts
+++ src/app/inicial/inicial.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { InicialComponent } from './inicial.component';
 
 describe('InicialComponent', () => {
--- src/app/inicial/inicial.component.ts
+++ src/app/inicial/inicial.component.ts
@@ -1,18 +1,8 @@
 import { Component } from '@angular/core';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatCardModule} from '@angular/material/card';
-import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
-import {NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-inicial',
   imports: [
-    MatToolbarModule,
-    MatCardModule,
-    MatButtonModule,
-    MatIconModule,
-    NgbCarouselModule
   ],
   templateUrl: './inicial.component.html',
   styleUrl: './inicial.component.css'
