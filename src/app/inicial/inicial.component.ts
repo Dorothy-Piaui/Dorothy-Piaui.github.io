2895215409930f4e54e2728a5df495189e84520a
Larissa Brasil <larissabrasil009@gmail.com>
2024-12-26 10:19:46 -0300

    feat: alterando styles and structure para a página inicial e adicionado bootstrap-icons

--- src/app/inicial/inicial.component.ts
+++ src/app/inicial/inicial.component.ts
@@ -3,6 +3,7 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
+import {NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-inicial',
@@ -10,7 +11,8 @@ import {MatIconModule} from '@angular/material/icon';
     MatToolbarModule,
     MatCardModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    NgbCarouselModule
   ],
   templateUrl: './inicial.component.html',
   styleUrl: './inicial.component.css'
